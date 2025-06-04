128290629a1612a35b20a450b9dded3ad9cbab5d
HyeonseoAhn <in3e6e@gmail.com>
2025-06-04 20:28:22 +0900

    추가 Gradio 예제

--- 5_Gradio4.py
+++ 5_Gradio4.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
-def handle_fruit(fruit):
-    return f'선택한 과일: {fruit}'
+def handle_fruit(fruit):  # 
+    return f'선택한 과일: {fruit}'   # 리턴된 게 outputs로 이동하는데 이때 outputs가 output_fruit를 가리키니까 output_fruit를 통해 출력됨
 
 with gr.Blocks() as demo:
     fruit_dropdown = gr.Dropdown(label="과일", choices=['사과', '오렌지', '바나나', '메론']) # Dropdown메서드: 선택지를 줌
